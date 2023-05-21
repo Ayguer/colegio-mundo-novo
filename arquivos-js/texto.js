29a2b97414b7962c4f2f610e66959b2b97bbea2b
Ayguer_Acabane <132191421+Ayguer@users.noreply.github.com>
2023-05-21 18:59:57 +0100

    def

--- arquivos-js/texto.js
+++ arquivos-js/texto.js
@@ -1,5 +1,5 @@
 const paragrafo = document.getElementById('text')
-const texto= 'Este é o colegio mundo novo temos como objecivo ensinar para um mundo melhor, garantimos sempre atividades em feriados, palestras e saidas para parques de diversoes cinemas, e sobre tudo o melhor ensino e um atendimento de qualidade. Conceteza no colegio mundo novo garantimos segurança, educaçao, diverçao e o melhor ensino academico possivel para o seu filho'
+const texto= 'Aulas de Nataçao, Capoeira, xadrez e Dança para alunos internos e externos. Os interessados devem contactar a secretaria.'
 const inter = 100;
 function escrever( a, b, c){
     const caracteres= b.split("").reverse()
